139978bfaead11bb60f2af14ece9178c6d789b6a
WendingZhao <2715511439@qq.com>
2024-11-27 23:16:25 +0800

    test commit

--- utils.py
+++ utils.py
@@ -3,3 +3,36 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+import json
+import os
+import numpy as np
+from skimage import io  # scikit-learn 包
+import matplotlib.pyplot as plt
+import pylab
+import cv2
+
+
+def get_tags(id: int) -> dict[str: int]:
+    '''
+    the method used now consume unnecessary computing source
+    need rebulid the atchitechture to make it more efficient
+    '''
+
+    json_path = r'./Annotations/val.json'
+    img_path = r'./Images/val/'
+    with open(json_path) as annos:
+        annotation_json = json.load(annos)
+    image_name = annotation_json['images'][id]['file_name']
+    category_dict = {category['id']: category['name'] for category in annotation_json['categories']}
+
+    ...
+
+
+def visualize(id: int, output_path) -> None:
+    ...
+
+
+if __name__ == '__main__':
+    ...
+    get_tags(2077)
